98e0108593493b6c5dc11cde28342c2ac6f79498
jullinhomn <70717085+jullinhomn@users.noreply.github.com>
2020-10-07 15:22:07 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,61 @@
+function gerar() {
+    let perguntas = [
+        'Eu nunca tive um sonho romântico com alguém que está aqui.' ,
+        'Eu nunca comprei um "brinquedo de estimulação".' ,
+        'Eu nunca fui pego me "brincando comigo mesmo".' ,
+        'Nunca me senti atraído(a) pelo pai/mãe de algum amigo.' ,
+        'Nunca ninguém dançou no meu colo.' ,
+        'Eu nunca enviei fotos sem roupa para a pessoa errada.' ,
+        'Eu nunca fingi prazer.' ,
+        'Nunca fiquei com alguém do mesmo gênero que eu.' ,
+        'Eu nunca fiquei com gêmeos.' ,
+        'Eu nunca fiquei com mais de uma pessoa ao mesmo tempo.' ,
+        'Eu nunca tirei a roupa enquanto dançava para alguém.' ,
+        'Eu nunca gravei vídeo enquanto estava ficando com alguém.' ,
+        'Eu nunca fui num clube para adultos.' ,
+        'Eu nunca mandei fotos sem roupa para alguém.' ,
+        'Eu nunca mandei a mesma fotografia para duas pessoas ao mesmo tempo.' ,
+        'Nunca quis ficar com um casal de amigos.' ,
+        'Eu nunca marquei de ficar com alguém do Tinder.' ,
+        'Eu nunca precisei ir no médico devido a um objeto estranho preso dentro do meu corpo.' ,
+        'Eu nunca tirei a roupa em uma festa.' ,
+        'Eu nunca tive medo de ter pego alguma DST.' ,
+        'Eu nunca tive uma fotografia imprópria vazada na internet.' ,
+        'Eu nunca tive um "amigo colorido" ou "amigo com benefícios".' ,
+        'Nunca vi meus pais em situações de intimidade extrema.' ,
+        'Eu nunca tive um sonho intímo com algum professor.' ,
+        'Eu nunca fiquei com alguém desta roda.' ,
+        'Eu nunca fiquei com alguém que eu nem sabia o nome.' ,
+        'Eu nunca fiquei com uma pessoa e depois nunca mais falei com ela.' ,
+        'Eu nunca fiquei com alguém na cama dos meus pais.' ,
+        'Eu nunca fiquei com alguém na rua.' ,
+        'Eu nunca usei o Tinder.' ,
+        'Eu nunca vi duas pessoas tendo relações íntimas.' ,
+        'Eu nunca dancei no colo de ninguém.' ,
+        'Eu nunca aluguei filme para maiores de idade em uma locadora.' ,
+        'Eu nunca fiquei com alguém na praia.' ,
+        'Nunca disse eu te amo apenas para ficar com alguém.' ,
+        'Nunca dormi enquanto estava no meio de uma situação de intimidade com alguém.' ,
+        'Eu nunca escondi uma marca no pescoço (feita por alguém com a boca) com uma blusa gola alta ou maquiagem.' ,
+        'Eu nunca fui pego assistindo filmes para maiores de idade com cenas picantes.' ,
+        'Eu nunca fiquei com alguém na universidade.' ,
+        'Eu nunca fiquei com alguém no banheiro de uma festa.' ,
+        'Eu nunca tive fantasias com os pés de alguém.' ,
+        'Nunca fiquei com alguém 20 anos mais velho que eu.' ,
+        'Eu nunca fiquei com alguém no banheiro de uma festa.' ,
+        'Nunca usei uma fantasia ousada.' ,
+        'Nunca flagrei meus vizinhos tendo relações íntimas.' ,
+        'Eu nunca usei o Tinder enquanto namorava.' ,
+        'Nunca usei alguma fruta ou legume para "aliviar o stress" do meu corpo.' ,
+        'Eu nunca quis ver meu parceiro ficando com outra pessoa.' ,
+        'Eu nunca enfiei o dedo em mim mesmo(a).' ,
+        'Nunca vi outro casal mantendo relações íntimas na minha frente.'
+    ];
+
+   var a = Math.random() * 49;
+   var sorteio = Math.round(a);
+    resultado.innerHTML = `${perguntas[sorteio]}`;
+
+
+
+}
